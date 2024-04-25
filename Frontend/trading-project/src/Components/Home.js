9f2695bea15e95588bd3909a34294c64282daeac
Ujwol Aryal <ujwolaryal@gmail.com>
2024-04-25 19:41:41 +0545

    Updated

---
+++ Frontend/trading-project/src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home()
+{
+    return (
+        <div>
+            <h1>This is Home Heading1</h1>
+            <h2>This is Home Heading 2</h2>
+        </div>
+    )
+}
+
+export default Home
