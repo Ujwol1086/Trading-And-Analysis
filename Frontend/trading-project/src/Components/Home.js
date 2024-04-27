16ff9baf1401293e62cc45f30bfc5896f3412408
Ujwol Aryal <ujwolaryal@gmail.com>
2024-04-27 14:53:45 +0545

    Updated

--- Frontend/trading-project/src/Components/Home.js
+++ Frontend/trading-project/src/Components/Home.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import Navbar from './Navbar'
+import "./../../node_modules/bootstrap/dist/css/bootstrap.min.css"
 
 function Home()
 {
     return (
-        <div>
-            <h1>This is Home Heading1</h1>
-            <h2>This is Home Heading 2</h2>
-        </div>
+        <>
+            {/* <Navbar /> */}
+            <div>
+                <h1>This is Home Heading1</h1>
+                <h2>This is Home Heading 2</h2>
+            </div>
+        </>
     )
 }
 
