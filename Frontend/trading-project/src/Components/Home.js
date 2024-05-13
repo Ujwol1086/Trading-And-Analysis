484b22f7edcd653c7428d2d3042cdb7b87ad0e91
Ujwol1086 <ujwolaryal@gmail.com>
2024-05-13 12:54:11 +0545

    Latest

--- Frontend/trading-project/src/Components/Home.js
+++ Frontend/trading-project/src/Components/Home.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import Navbar from './Navbar'
-import "./../../node_modules/bootstrap/dist/css/bootstrap.min.css"
 
 function Home()
 {
     return (
         <>
-            {/* <Navbar /> */}
-            <div>
-                <h1>This is Home Heading1</h1>
-                <h2>This is Home Heading 2</h2>
+            <Navbar />
+            <div className=''>
             </div>
         </>
     )
--- Frontend/trading-project/src/Components/Navbar.js
+++ Frontend/trading-project/src/Components/Navbar.js
@@ -1,34 +1,56 @@
 import React from 'react'
-import { Container, Nav, NavDropdown } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 function Navbar()
 {
     return (
-        <>
-            <Navbar expand="lg" className="bg-body-tertiary">
-                <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">
-                                    Another action
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">
-                                    Separated link
-                                </NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </>
+        <div>
+            <nav className='flex justify-between items-center w-full px-10 py-2 bg-neutral-400'>
+                <img src="./src/Images/Logo.jpg" alt='Not Found' className='w-16 h-16 rounded-full' />
+                <input type='text' name='search' placeholder='Search For Company' className='w-80 h-10 p-2 rounded-md text-black focus:outline-cyan-200' />
+                <ul className='flex gap-10'>
+                    <li className='p-2 rounded-md hover:bg-slate-500 ease-in-out duration-150 '><Link to="/">Login</Link></li>
+                    <li className='p-2 rounded-md hover:bg-slate-500 ease-in-out duration-150 '><Link to="/">Register</Link></li>
+                    <li className='p-2 rounded-md hover:bg-slate-500 ease-in-out duration-150 '><Link to="/">Contact us</Link></li>
+                    <li className='p-2 rounded-md hover:bg-slate-500 ease-in-out duration-150 '><Link to="/">About</Link></li>
+                </ul>
+            </nav>
+            <nav className='flex justify-between px-10 h-10 bg-orange-500 items-center'>
+                <ul className='flex gap-10'>
+                    <li><Link to="/">Home</Link></li>
+                    <li>
+                        <select className='bg-transparent'>
+                            <option>News</option>
+                        </select>
+                    </li>
+                    <li>
+                        <select className='bg-transparent'>
+                            <option>Market</option>
+                        </select>
+                    </li>
+                    <li>
+                        <select className='bg-transparent'>
+                            <option>IPO/FPO</option>
+                        </select>
+                    </li>
+                    <li>
+                        <select className='bg-transparent'>
+                            <option>Services</option>
+                        </select>
+                    </li>
+                    <li>
+                        <select className='bg-transparent'>
+                            <option>Tools</option>
+                        </select>
+                    </li>
+                    <li>
+                        <select className='bg-transparent'>
+                            <option>Analysis</option>
+                        </select>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     )
 }
 
